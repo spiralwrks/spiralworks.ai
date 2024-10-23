50642851a368f486f09393a16d20cfee942d9614
Aishik Sanyal <cytroyd@gmail.com>
2024-10-23 16:41:45 -0300

    routing fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import ReadingGroup from './components/ReadingGroup';
